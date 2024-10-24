f556cacb282ed04c7bc98c5fdb6d681cf9136cea
Abhay Phutela <abhayarora071@gmail.com>
2024-10-24 14:38:23 +0530

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../Header/Header';
-import Footer from '../Footer/footer';
+import Footer from '../Footer/Footer';
 import Reviews from '../reviews/Reviews';
 import Plans from '../plans/Plans'
 import Recipe from '../Recipe/Recipe';
